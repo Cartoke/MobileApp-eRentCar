b06e2cdeca4f180d0fa0b717d73f86636d47b404
2CTO <u201521691@upc.edu.pe>
2022-06-02 16:40:12 -0500

    feat: steps added
    username & password was added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: Login(),
+    );
   }
 }
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image(
+            image: AssetImage("lib/assets/background.jpg"),
+            fit: BoxFit.cover,
+            color: Colors.black26,
+            colorBlendMode: BlendMode.luminosity,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage("lib/assets/logo.png"),
+              ),
+              Form(
+                child: Theme(
+                  data: ThemeData(
+                    brightness: Brightness.dark, primarySwatch: Colors.teal,
+                    inputDecorationTheme: InputDecorationTheme(
+                      labelStyle: TextStyle(
+                        color: Colors.lightBlue,
+                        fontSize: 20
+                      )
+                    )
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(30),
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Enter e-mail:"
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            labelText: "Enter password:"
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(top: 30)
+                        ),
+                        MaterialButton(
+                            height: 40,
+                            minWidth:60,
+                            color: Colors.lightBlue,
+                            textColor: Colors.white,
+                            child: Icon(FontAwesomeIcons.signInAlt),
+                            onPressed: () => {},
+                            splashColor: Colors.blueAccent,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
