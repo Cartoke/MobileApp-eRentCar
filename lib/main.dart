368ffeb4f809cbc1d007ff0d6ab6cf07258f78ec
Juan de Dios Quiroz <quirozrodriguezj@gmail.com>
2022-06-02 23:45:07 -0500

    feat: My vehicles view added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: myCars()
+    );
+  }
+}
+
+class myCars extends StatefulWidget {
+  const myCars({Key? key}) : super(key: key);
+  @override
+  State<myCars> createState() => _myCarsState();
+}
+
+class _myCarsState extends State<myCars> {
+  final List<String> cars = <String>[];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text('My Vehicles'),
+      ),
+      body: cars.length > 0 ? buildCars() :
+        const Center(
+          child: Text('No cars added yet'),
+        ),
+      floatingActionButton: showAddButton()
+    );
   }
+
+  Widget buildCars(){
+    return ListView.separated(
+      itemCount: cars.length,
+      itemBuilder: (BuildContext context, int index){
+        return buildRow(cars[index], index);
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider(
+          thickness: 1,
+          color: Colors.black26,
+        );
+      },
+    );
+  }
+
+  Widget buildRow(String car, int index){
+    return ListTile(
+      leading: const Icon(Icons.directions_car),
+      title: Text(car),
+      subtitle: const Text('Car Description'),
+      onLongPress: (){
+        setState(() {
+          cars.removeAt(index);
+        });
+      },
+    );
+  }
+
+  showAddButton(){
+    return FloatingActionButton(
+      backgroundColor: Colors.blueAccent,
+      child: const Icon(Icons.add),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => addCar()),
+        );
+      },
+    );
+  }
+
+  addCar() {
+    final textController = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text('Add Car'),
+      ),
+      body:
+        Center(
+          child: TextField(
+            controller: textController,
+            decoration: const InputDecoration(
+              labelText: 'Car Name',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blueAccent,
+        child: const Icon(Icons.save),
+        onPressed: () {
+          setState(() {
+            cars.add(textController.text);
+            textController.clear();
+          });
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+
 }
