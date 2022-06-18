d6ae3dd0741c4c3dc6d76668c5df46c84c4e83d9
2CTO <u201521691@upc.edu.pe>
2022-06-17 21:55:37 -0500

    feat: login view changed

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+
       home: login_page(),
     );
   }
